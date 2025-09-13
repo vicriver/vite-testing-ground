1e2653de50a9b3c7c9c1e4352d5da6d9954d33c4
VikHBoss <36492341+VikHBoss@users.noreply.github.com>
2025-09-12 21:12:24 -0400

    added cards

---
+++ src/views/Main.tsx
@@ -0,0 +1,68 @@
+
+
+export default function Main() {
+	const count = 10;
+	const numbers : string[] = [];
+	const cards = [
+		{
+		title: "First Card",
+		content: "Ace here"
+	},
+	{
+		title: "Jack",
+		content: "Jack up"
+	},
+	{
+		title: "Joker",
+		content: "Joke's on you!"
+	},
+	{
+		title: "Jack",
+		content: "Jack up"
+	},
+	{
+		title: "Joker",
+		content: "Joke's on you!"
+	},
+	{
+		title: "Jack",
+		content: "Jack up"
+	},
+	{
+		title: "Joker",
+		content: "Joke's on you!"
+	},
+	{
+		title: "Jack",
+		content: "Jack up"
+	},
+	{
+		title: "Joker",
+		content: "Joke's on you!"
+	},
+	{
+		title: "Jack",
+		content: "Jack up"
+	},
+	{
+		title: "Joker",
+		content: "Joke's on you!"
+	},
+	{
+		title: "Jack",
+		content: "Jack up"
+	},
+	{
+		title: "Joker",
+		content: "Joke's on you!"
+	},
+];
+	for (let i = 0; i < count; i++ ) { 
+		numbers.push(`Page number ${i + 1}`)
+	}
+
+	console.log(numbers);
+  return (
+      <div className="flex flex-wrap gap-2" >{cards.map(card => <div className="bg-gray-200 text-black h-60 w-36 rounded-md p-4 origin-bottom hover:bg-red-300 transition-all active:rotate-15 flex flex-col justify-between border-10 border-white">{card.title} {card.content}</div>)}</div>
+  )
+}
